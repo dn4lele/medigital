a18af4460cd9f08cd87a50bd4d04347af77927c2
dn4lele <danielmaier4312@gmail.com>
2023-05-04 09:08:39 +0300

    9:08

---
+++ models/database.js
@@ -0,0 +1,18 @@
+import Sequence from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+
+const connection =new Sequelize(
+    process.env.DB_NAME, //database name
+    process.env.DB_USERNAME, // userr
+    process.env.DB_PASSWORD,
+    {
+        dialect: mysql,
+        host: localhost
+    }
+)
+
+export default sequelize;
